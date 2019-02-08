df319024202086acbe2ad3b4f852275b4c596727
Ben Vankat <bvankat@gmail.com>
2019-02-08 13:26:18 -0600

    Tweaks to simplify scraping for multiple teams/years

--- drought-finder.py
+++ drought-finder.py
@@ -5,7 +5,9 @@ import json
 
 # define the years you want to work on
 years = ["2019"]
-team = "Nebraska"
+
+# to get another team, change lines 10, 47, 61, 92, 112
+team = "Ohio+St."
 
 
 #loop through each year in the list above
@@ -42,7 +44,7 @@ for year in years:
     
         game_date = game["ymd"]
         
-        if game["team1"] == "Nebraska":
+        if game["team1"] == "Ohio St.": # in game["input"] team names aren't slugified (e.g. "Ohio State")
             site = "Road"
             game_opponent = game["team2"]
         else: 
@@ -56,7 +58,7 @@ for year in years:
         game["input"] = sorted(game["input"], key=lambda k: k["TL"], reverse=True)
         
         #find all the scoring possessions
-        if game["team1"] == "Nebraska":
+        if game["team1"] == "Ohio St.":
             nebraska = [p for p in game["input"] if p["VSc"] == 1] #Nebraska is away team
             opponent = [p for p in game["input"] if p["HSc"] == 1]
     
@@ -87,7 +89,7 @@ for year in years:
         # sort possessions by drought length
         nebraska_sorted = sorted(nebraska, key=lambda k: k["Dr"], reverse=True)
         opponent_sorted = sorted(opponent, key=lambda k: k["Dr"], reverse=True)
-        print("Longest Nebraska drought: %.2f" % nebraska_sorted[0]["Dr"])
+        print("Longest Ohio State drought: %.2f" % nebraska_sorted[0]["Dr"])
         print("Longest " + game_opponent + " drought: %.2f" % opponent_sorted[0]["Dr"])
         
         nebraska_droughts = [ p["Dr"] for p in nebraska_sorted ]
@@ -107,9 +109,9 @@ for year in years:
         
         print("3+ minute droughts for ", team, ": ", three_plus)
         
-        game_droughts["nebraska"] = nebraska_droughts
+        game_droughts["ohio-state"] = nebraska_droughts
         game_droughts["opponent"] = opponent_droughts
-        game_droughts["neb-droughts-by-length"] = droughts_by_length
+        game_droughts["droughts-by-length"] = droughts_by_length
                 
         all_droughts.append({ "date": game_date, "site": site, "opponent": game_opponent, "droughts": game_droughts })
         
